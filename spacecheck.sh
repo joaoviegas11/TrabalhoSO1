90135237a9ae0280939fd676339715ef1d8c389e
DJI28 <diogodomingues04@ua.pt>
2023-10-28 20:58:49 +0100

    funcao search_dir implementada

--- spacecheck.sh
+++ spacecheck.sh
@@ -21,3 +21,21 @@ if [[ "$vn"  == "1" ]]; then
 echo "$vn $vr  $vd "
 echo ("teste")
 echo ("teste1")
+
+function arg_check(){
+    if [ $# -lt 1 ]; then
+        echo "Not enough arguments."
+        echo "Usage: spacecheck.sh directory"
+        return
+    fi
+}
+
+function search_dir(){
+    for dir in "$@"; do
+        if [ -d "$dir" ]; then
+            find "$dir" -type d -exec du -s {} \; 2>/dev/null | awk '{print $1, $2}'
+        else
+            echo "$dir is not a directory"
+        fi
+    done
+}
