49871c0a699ddf34c0185a44236c0840155d6141
DJI28 <diogodomingues04@ua.pt>
2023-10-31 20:52:48 +0000

    pequna alteração a spacecheck para se definir o script_dir apenas uma vez

--- spacecheck.sh
+++ spacecheck.sh
@@ -1,8 +1,10 @@
 source ./inputParametros.sh
 
+script_dir=$(pwd)
+
 function search_dir(){
-    script_dir=$(pwd)
     local result=()
+    
     for dir in "${!#}"; do
         if [ -d "$dir" ]; then
             result+=( $(find "$dir" -type d -exec printf "%s\n" "$script_dir/{}" \; 2>/dev/null) )
@@ -14,9 +16,8 @@ function search_dir(){
 
 function search_files(){
     local directories=($(search_dir "$@"))
-    script_dir=$(pwd)
     # if no options
-    echo "SIZE NAME $(date +'%Y%m%d') $*"  #falta o resto dos argumentos de chamada
+    echo "SIZE NAME $(date +'%Y%m%d') $*"
     for dir in "${directories[@]}"; do
         cd $dir
             size=$(find . -type f -exec du -s {} \; 2>/dev/null | awk '{s+=$1} END {print s}')
