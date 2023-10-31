f71dca4e1681271d2db0f20bfbb7f6b271016d4d
joaoviegas11 <joaoviegas11@ua.pt>
2023-10-31 00:18:11 +0000

    input de parametros melhorado

--- inputParametros.sh
+++ inputParametros.sh
@@ -1,97 +1,73 @@
 #!n/bin/bash
 
-
-index=1
-# Loop para percorrer todos os parâmetros
-while [ "$index" -le "$#" ]; do
-    #parametro="${!index}"
-    #echo "Parâmetro $index: $parametro"
-    #Caso ultimo elemento
-    case ${!index} in
-    "-n")
-        echo n;
-        index=$((index + 1));
-        #echo "${!index}"
-        #if [[ "\"${!index}\"" =~ ^\".*\"$ ]]; then
-        #    echo "A palavra está entre aspas duplas."
-        #else
-        #    echo "A palavra não está entre aspas duplas."
-        #fi
-    ;;
-    
-    "-d")
-        echo d;
-        index=$((index + 1));
-        #echo "${!index}"
-        #if [[ "\"${!index}\"" =~ ^\".*\"$ ]]; then
-        #    echo "A palavra está entre aspas duplas."
-        #else
-        #    echo "A palavra não está entre aspas duplas."
-        #fi
-    ;;
-    
-    "-s")
-        #echo s;
-        index=$((index + 1));
-        #echo "${!index}"
-        if [[ "${!index}" =~ ^[0-9]+$ ]]; then
+ordered=0
+limit=0
+reverse=0
+echo "SIZE NAME $(date +%Y%m%d) $*"
+while getopts ":n:d:s:l:ar" opt; do
+case $opt in
+    n)
+        echo "Opção n, argumento: $OPTARG"
+        # Implemente a lógica para a opção -n aqui
+        ;;
+    d)
+        echo "Opção d, argumento: $OPTARG"
+        # Implemente a lógica para a opção -d aqui
+        ;;
+    s)
+        echo "Opção s, argumento: $OPTARG"
+        if [[ "$OPTARG" =~ ^[0-9]+$ ]]; then
             echo "A variável é um número."
         else
             echo "A variável não é um número."
             exit 1
         fi
-    ;;
-
-    "-a")
-        echo a;
-        sort -n ./testetemp.txt >./temp1
-    ;;
-    "-l")
-        index=$((index + 1));
-        if [[ "${!index}" =~ ^[0-9]+$ ]]; then
-            echo "A variável é um número."
+        ;;
+    a)
+        echo "Opção a"
+        ordered=1
+        #sort -n ./testetemp.txt >./temp1
+        # Implemente a lógica para a opção -a aqui
+        ;;
+    l)
+        echo "Opção l, argumento: $OPTARG"
+        if [[ "$OPTARG" =~ ^[0-9]+$ ]]; then
+            limit=$OPTARG
         else
             echo "A variável não é um número."
-        fi
-        tail -"${!index}" ./temp1 >./temp
-    ;;
-    "-r")
-        echo r;
-    ;;
-    ${!#})
-        echo ultimo;
-        if [ ! -d ${!#} ]; then
-            echo "$(pwd)/${!#} é um diretório válido."
             exit 1
         fi
-    ;;
-     * )
-    echo "Parametro invalido"
-    exit 1
-    
-    
- ;;
+        #tail -"$OPTARG" ./temp1 >./temp
+        # Implemente a lógica para a opção -l aqui
+        ;;
+    r)
+        echo "Opção r"
+        reverse=1
+        ;;
+    \?)
+        echo "Opção inválida: -$OPTARG" >&2
+        exit 1
+        ;;
+    :)
+        echo "A opção -$OPTARG requer um argumento." >&2
+        exit 1
+        ;;
 esac
-    index=$((index + 1))
 done
 
-
-
-
-
-function search_dir(){
-    for dir in "${!#}"; do
-        if [ -d "$dir" ]; then
-            find "$dir" -type d -exec du -s {} \; 2>/dev/null | awk '{print $1, $2}'>testetemp.txt
-        else
-            echo "$dir is not a directory"
-        fi
-    done
-    
-
-}
-
-    
-
-    cat ./temp
-    rm ./temp ./temp1
+if [[ $reverse -eq 1 ]]; then 
+    sort -r ./testetemp.txt >./temp
+    cat ./temp > ./testetemp.txt
+    rm temp
+    fi
+if [[ $ordered -eq 1 ]]; then 
+    sort -n ./testetemp.txt >./temp
+    cat ./temp > ./testetemp.txt
+    rm temp
+    fi
+if [[ $limit -gt 0 ]]; then 
+    tail -"$limit" ./testetemp.txt >./temp
+    cat ./temp > ./testetemp.txt
+    rm temp
+    fi
+cat ./testetemp.txt
